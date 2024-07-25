140c1a4d53f08b32d2a4064869c7fb1a0f069063
Samuel Lee <samuel.lee9525@gmail.com>
2024-07-25 13:28:04 +0800

    test commit, no changes act made

--- flaskr/confirm.py
+++ flaskr/confirm.py
@@ -2,7 +2,6 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, session, current_app
 )
 from werkzeug.exceptions import abort
-
 from .auth import login_required
 from .initdb import get_db
 
@@ -19,18 +18,22 @@ def add():
             pw = pw_dict['cipher_text']
             pw_id = session.get('pw_id')
             dbconn = get_db()
+            
             with dbconn.cursor() as cur:
                 SQL_delete = 'DELETE FROM pw WHERE email = %s AND pw_id = %s'
                 SQL_insert = 'INSERT INTO pw (email, pw, pw_id, salt, iv) VALUES (%s, %s, %s, %s, %s)' 
+                
                 cur.execute(
                     SQL_delete, (email, pw_id) 
                 )
                 cur.execute(
                     SQL_insert, (email, pw, pw_id, pw_dict['salt'], pw_dict['iv']) 
                 )
+                
                 dbconn.commit()
                 cur.close()
                 dbconn.close()
+            
             flash("Password Updated")
             return redirect(url_for("menu.home"))
     return render_template('menu/confirm.html', goto = url_for('menu.home'), message = message)
@@ -44,6 +47,7 @@ def delete():
     if request.method == 'POST':
         if request.form['answer'] == "Ok":
             dbconn = get_db()
+            
             with dbconn.cursor() as cur:
                 SQL_delete = 'DELETE FROM pw WHERE id = %s'
                 for num in arr:
@@ -51,6 +55,7 @@ def delete():
                 dbconn.commit()
                 cur.close()
                 dbconn.close()
+            
             flash("Password(s) Deleted")
             return redirect(url_for("menu.home"))
     return render_template('menu/confirm.html', goto = url_for('menu.delete'), message = message)
