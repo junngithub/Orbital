795d6c6bb9f881e4a6460e02d9f89a28b6fdeec9
Samuel Lee <samuel.lee9525@gmail.com>
2024-07-25 13:44:18 +0800

    added comments to explain

--- flaskr/start.py
+++ flaskr/start.py
@@ -6,6 +6,8 @@ bp = Blueprint('start', __name__)
 
 @bp.route('/')
 def start():
+    # if user is logged in, take user to home page
     if g.user is not None:
         return(redirect(url_for("menu.home")))
+    # else take user to welcome page
     return render_template("start.html")
